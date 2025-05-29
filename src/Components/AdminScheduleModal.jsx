fbcf13617877b1e317f1ade515954b5f9cfe065d
tashiwangyel <bajayfs@gmail.com>
2025-05-29 12:37:03 +0600

    updates on frontend

--- src/Components/AdminScheduleModal.jsx
+++ src/Components/AdminScheduleModal.jsx
@@ -661,7 +661,7 @@ function AdminScheduleModal({
         </span>
         <div className="form-title">Booking Details</div>
 
-        <form onSubmit={handleSubmit}>
+        <form>
           <p className="booking-break-header">Client/Agent Details</p>
 
           <div className="booking-form-group">
